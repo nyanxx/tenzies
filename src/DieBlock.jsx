f7e6aa7ba0589c0840124f0d9b95a44b77668c0a
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2026-01-20 15:10:28 +0530

    done: Tenzies - map array to Die components challenge

--- src/DieBlock.jsx
+++ src/DieBlock.jsx
@@ -6,6 +6,39 @@ export default function DieBlock() {
   const [rolled, setRolled] = React.useState(0);
   const [lockedNumbers, setLockedNumbers] = React.useState([]);
 
+  /**
+   * Tenzies: Map array to Die components
+   * Challenge:
+   *
+   * Create state to hold our array of numbers. (Initialize
+   * the state by calling our `generateAllNewDice` function so it
+   * loads all new dice as soon as the app loads)
+   *
+   * Map over the state numbers array to generate our array
+   * of Die components and render those in place of our
+   * manually-written 10 Die elements.
+   */
+
+  const [randomArr, setRandomArr] = React.useState(generateAllNewDice());
+  console.log(randomArr);
+  function generateAllNewDice() {
+    return Array.from({ length: 10 }).map(() => Math.ceil(Math.random() * 6));
+  }
+
+  // React.useEffect(() => {
+  //   setRandomArr(generateAllNewDice());
+  // }, [rolled]);
+
+  /** map over dice here */
+  const dieElements = randomArr.map((dieNum, index) => (
+    <Die
+      key={index}
+      setLockedNumbers={setLockedNumbers}
+      randomNumber={dieNum}
+      rolled={rolled}
+    />
+  ));
+
   function handleRoll() {
     console.log("Rolled");
     // setRolled((prevState) => !prevState);
@@ -21,17 +54,19 @@ export default function DieBlock() {
   // timer
 
   //   console.log(lockedNumbers);
+
   return (
     <>
       <div className="die-buttons-block">
-        {Array.from({ length: 10 }).map((_, index) => (
+        {dieElements}
+        {/* {Array.from({ length: 10 }).map((_, index) => (
           <Die
             key={index}
             setLockedNumbers={setLockedNumbers}
             randomNumber={Math.ceil(Math.random() * 6)}
             rolled={rolled}
           />
-        ))}
+        ))} */}
       </div>
       <button
         onClick={handleRoll}
