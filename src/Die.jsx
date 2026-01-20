f889d8f8df0c2f471e0a5e99f5757fdeb165ff08
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2026-01-20 16:37:43 +0530

    done: Tenzies - Change dice to objects challenge

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,29 +1,5 @@
 import React from "react";
 
-export default function Die({ randomNumber, setLockedNumbers, rolled }) {
-  //   const randomNumber = Math.floor(Math.random() * 10);
-
-  const [dieNumber, setDieNumber] = React.useState();
-  const [isLocked, setIsLocked] = React.useState(false);
-  const [lockedNumber, setLockedNumber] = React.useState();
-
-  React.useEffect(() => {
-    // console.log("use effect");
-    setDieNumber(randomNumber);
-  }, [rolled]);
-
-  function lockDie(event) {
-    !isLocked &&
-      (setLockedNumber(dieNumber),
-      console.log(`${dieNumber} is locked!`),
-      event.currentTarget.classList.add("locked"),
-      setLockedNumbers((prevArr) => [...prevArr, dieNumber]),
-      setIsLocked((prevState) => !prevState));
-  }
-
-  return (
-    <div onClick={lockDie} className="die">
-      {!isLocked ? dieNumber : lockedNumber}
-    </div>
-  );
+export default function Die(props) {
+  return <div className="die">{props.value}</div>;
 }
--- src/DieBlock.jsx
+++ src/DieBlock.jsx
@@ -1,72 +1,57 @@
 import React from "react";
 import Die from "./Die";
+import { nanoid } from "nanoid";
 
 export default function DieBlock() {
-  //   const [rolled, setRolled] = React.useState(true)
-  const [rolled, setRolled] = React.useState(0);
-  const [lockedNumbers, setLockedNumbers] = React.useState([]);
+  /**
+   * Tenzies: Change dice to objects
+   * Challenge: Update the array of numbers in state to be
+   * an array of objects instead. Each object should look like:
+   * { value: <random number>, isHeld: false }
+   *
+   * Making this change will break parts of our code, so make
+   * sure to update things so we're back to a working state
+   */
 
-  const [randomArr, setRandomArr] = React.useState(generateAllNewDice());
-  console.log(randomArr);
-  function generateAllNewDice() {
-    return Array.from({ length: 10 }).map(() => Math.ceil(Math.random() * 6));
+  const [dieProperties, setDieProperties] = React.useState(
+    generateAllNewDiceProperty(),
+  );
+
+  console.log(dieProperties);
+  function generateAllNewDiceProperty() {
+    return Array.from({ length: 10 }).map(() => {
+      return {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      };
+    });
   }
 
-  /** map over dice here */
-  const dieElements = randomArr.map((dieNum, index) => (
-    <Die
-      key={index}
-      setLockedNumbers={setLockedNumbers}
-      randomNumber={dieNum}
-      rolled={rolled}
-    />
+  const dieElements = dieProperties.map((dieProperty) => (
+    <Die key={dieProperty.id} value={dieProperty.value} />
   ));
-
-  /**
-   * Tenzies: Roll dice button
-   * Challenge: Create a `Roll Dice` button that will re-roll
-   * all 10 dice
-   *
-   * Clicking the button should generate a new array of numbers
-   * and set the `dice` state to that new array (thus re-rendering
-   * the array to the page)
-   */
 
   function handleRoll() {
     console.log("Rolled");
-    setRandomArr(generateAllNewDice());
-    // setRolled((prevState) => !prevState);
-    setRolled((prevState) => prevState + 1);
+    setDieProperties(generateAllNewDiceProperty());
   }
 
-  let roll_newgame = "Roll";
-  if (lockedNumbers.length === 10 && new Set(lockedNumbers).size === 1) {
-    alert("you won");
-    roll_newgame = "New Game";
-  }
-
-  // timer
-  //   console.log(lockedNumbers);
+  // let roll_newgame = "Roll";
+  // if (lockedNumbers.length === 10 && new Set(lockedNumbers).size === 1) {
+  //   alert("you won");
+  //   roll_newgame = "New Game";
+  // }
 
   return (
     <>
-      <div className="die-buttons-block">
-        {dieElements}
-        {/* {Array.from({ length: 10 }).map((_, index) => (
-          <Die
-            key={index}
-            setLockedNumbers={setLockedNumbers}
-            randomNumber={Math.ceil(Math.random() * 6)}
-            rolled={rolled}
-          />
-        ))} */}
-      </div>
+      <div className="die-buttons-block">{dieElements}</div>
       <button
         onClick={handleRoll}
         className="roll-newgame-button"
         id="roll-newgame"
       >
-        {roll_newgame}
+        {/* {roll_newgame} */}Roll
       </button>
     </>
   );
